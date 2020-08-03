198d23dc958040c96a727a4eb464dc9c8420d48f
bebyx <bebyx@protonmail.com>
2020-08-03 09:24:25 +0300

    Add comments to fe deploy script

--- fe.sh
+++ fe.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
+# Update CentOS8 and install needed packages
 dnf update -y
 dnf install httpd -y
 
+# Start Apache web server
 systemctl start httpd
 
+# Open http (port 80) for public
 firewall-cmd --permanent --add-service=http
 firewall-cmd --reload
 
+# Create and assign frontend Apache folder
 mkdir /var/www/eschool
 chown -R vagrant:vagrant /var/www/eschool/
 
+# Unarchive frontend files into Apache folder
 tar -xjvf /vagrant/eschool.tar.bz2 -C /var/www/eschool/
 
+# Add VirtualHost for the web instance
 cat <<EOF > /etc/httpd/conf.d/eschool.conf
 <VirtualHost *:80>
     #ServerName example.com
@@ -30,4 +36,5 @@ cat <<EOF > /etc/httpd/conf.d/eschool.conf
 </VirtualHost>
 EOF
 
+# Restart Apache to enable new config
 systemctl restart httpd
