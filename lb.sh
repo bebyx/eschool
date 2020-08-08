cdc4e62a8da98091b54d44bd8c277338b667a1f9
bebyx <bebyx@protonmail.com>
2020-08-08 18:16:15 +0300

    Remake Load Balancer from Apache to nginx

--- lb.sh
+++ lb.sh
@@ -1,34 +1,29 @@
 #!/bin/sh
 
 dnf update -y
-dnf install httpd -y
+dnf install nginx -y
 
 firewall-cmd --permanent --add-service=http
 firewall-cmd --reload
 setenforce 0
 
-cat<<EOF > /etc/httpd/conf.d/lb.conf
-<VirtualHost *:80>
+cat<<EOF > /etc/nginx/conf.d/lb.conf
+upstream app {
+  ip_hash;
+  server $APP1_IP$PORT;
+  server $APP2_IP$PORT;
+}
 
-Header add Set-Cookie "ROUTEID=.%{BALANCER_WORKER_ROUTE}e; path=/" env=BALANCER_ROUTE_CHANGED
+server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
 
-<Proxy *>
-    Order deny,allow
-    Allow from all
-</Proxy>
-
-<Proxy balancer://mycluster>
-    BalancerMember http://$APP1_IP$PORT route=backend-1 enablereuse=On
-    BalancerMember http://$APP2_IP$PORT route=backend-2 enablereuse=On
-    ProxySet lbmethod=bytraffic
-    ProxySet stickysession=ROUTEID
-</Proxy>
-
-    ProxyPreserveHost On
-
-    ProxyPass "/" "balancer://mycluster/"
-    ProxyPassReverse "/" "balancer://mycluster/"
-</VirtualHost>
+        location / {
+            proxy_pass http://app;
+        }
+}
 EOF
 
-systemctl start httpd
+sed -i "s|\s*default_server||g" /etc/nginx/nginx.conf
+
+systemctl start nginx
