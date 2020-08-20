10233d116c85a1f85b84f5eb3a1ea4a40ecfd5c3
bebyx <bebyx@protonmail.com>
2020-08-20 15:29:49 +0300

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,6 @@ vagrant up lb_be && notify-send "LoadBalancer (Backend) has been deployed"
 vagrant up fe1 && notify-send "Frontend-1 has been deployed"
 vagrant up fe2 && notify-send "Frontend-2 has been deployed"
 vagrant up lb_fe && notify-send "LoadBalancer (Frontend) has been deployed"
+vagrant up jenkins && notify-send "Jenkins server has been deployed"
 
 notify-send "Web app has been completely deployed to Google Cloud"
